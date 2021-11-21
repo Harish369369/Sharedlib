01cf11d921bc9b0d46d85a7947773d4fcfdfbaeb
Harish D V <91498096+Harish369369@users.noreply.github.com>
2021-11-21 16:42:12 +0530

    Update tomcat-deploy.groovy

--- vars/tomcat-deploy.groovy
+++ vars/tomcat-deploy.groovy
@@ -1,5 +1,5 @@
 def call(ip,credsid,myweb){
-  sshagent(["${credsid}"]) {
+  sshagent(["${credsid}]") {
     sh "scp -o StrictHostKeyChecking=no target/${warname}*.war ec2-user@${ip}:/opt/tomcat8/webapps/"
   sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
 }
