d511ebac1f2d01cd294c30e65f1e050cdd51f18e
Harish D V <91498096+Harish369369@users.noreply.github.com>
2021-11-21 15:45:11 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,11 @@
 def call(name){
 echo "Hey ${name}, Welcome to Jenkins Shared libraries"
 }
+
+def call(name){
+  echo "Hey ${name}, Go to hell"
+}
+
+def call(name){
+  echo "Hey ${name}, What are you doing?"
+}
