d7371fa79dae32d04a707f3f5a292413b2948447
Harish D V <91498096+Harish369369@users.noreply.github.com>
2021-11-22 13:38:25 +0530

    Update mavennexusupload.groovy

--- vars/mavennexusupload.groovy
+++ vars/mavennexusupload.groovy
@@ -5,4 +5,5 @@ def call(conf){
   def artifactId = pomFile.artifactId
   def type = pomFile.packaging
   def groupId = pom.File.groutId
-  nexusArtifactUploader artifacts: [[artifactId: "${artifactId}", classifier: '}', file: "target/${artifactId}-${version}.war", type: type]], credentialsId: 'Nexus3', groupId: groupId, nexusUrl: '13.232.36.145:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: repoName, version: "${version}"
+  nexusArtifactUploader artifacts: [[artifactId: "${artifactId}", classifier: '', file: "target/${artifactId}-${version}.war", type: type]], credentialsId: 'Nexus3', groupId: groupId, nexusUrl: '13.232.36.145:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: repoName, version: "${version}"
+}
