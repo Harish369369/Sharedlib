221dc79977ef498c457fe32af5ff40b310a98460
Harish D V <91498096+Harish369369@users.noreply.github.com>
2021-11-22 13:14:53 +0530

    Create mavennexusupload.groovy

---
+++ vars/mavennexusupload.groovy
@@ -0,0 +1,9 @@
+def call(conf){
+  def repoName = conf[]
+  def pomFile = readMavenPom file: 'pom.xml'
+  def version = pomFile.version
+  def artifact Id = pomFile.artifactId
+  def type = pomFile.packaging
+  def groupId = pom.File.groutId
+  nexusArtifactUploader artifacts: [[artifactId: "${artifactId}", classifier: '', file: "target/${artifactId}-${version}.war", type: type]], credentialsId: 'Nexus3', groupId: groupId, nexusUrl: '13.232.36.145:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: repoName, version: "${version}"
+  
